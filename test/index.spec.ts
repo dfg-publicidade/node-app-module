d12daac5b7594e8dd966c36bfa561ad9fa080013
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2021-03-09 11:56:41 -0300

    Recompilação 09/03/2021 11:11:41

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,10 +1,17 @@
 import chai, { Chai } from 'chai';
 import { describe, it } from 'mocha';
+import App from '../src/index';
 
 /* Tests */
 const expect: Chai.ExpectStatic = chai.expect;
 
-describe('Teste das funcionalidades gerais da API', () => {
-    it('deve inicalizar o servidor da API corretamente', async () => {
+describe('index.ts', (): void => {
+    it('constructor', async (): Promise<void> => {
+        const app: App = new App({
+            appInfo: undefined,
+            config: undefined,
+            connectionName: undefined,
+            db: undefined
+        });
     });
 });

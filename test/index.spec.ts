43fddcb9fc657490c6d9e7ea47e71b4dd9666f6e
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2021-03-09 08:44:36 -0300

    Verificação de erros na busca dos dados de inscrição vindos do RM

---
+++ test/index.spec.ts
@@ -0,0 +1,10 @@
+import chai from 'chai';
+import { } from 'mocha';
+
+/* Tests */
+const expect: Chai.ExpectStatic = chai.expect;
+
+describe('Teste das funcionalidades gerais da API', () => {
+    it('deve inicalizar o servidor da API corretamente', async () => {
+    });
+});
